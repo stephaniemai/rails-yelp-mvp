228ecfc837ffc393b59fb2b2106b18d19eac6d28
stephaniemai <stephaniemai.jacques@gmail.com>
2018-11-21 12:02:12 +0100

    models created with validations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+////= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require_tree .
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, :rating, presence: true
+  validates :rating, numericality: {
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 5
+  }
+end
